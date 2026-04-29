5cef288a512d8780e08cb739e08ec5cc2fa4cb2b
mhmdmetwally <mhmdedak@gmail.com>
2026-04-29 18:22:56 +0200

    deploy

--- app.js
+++ app.js
@@ -1,41 +1,38 @@
+const express = require('express');
+const cors = require('cors');
+const mongoose = require('mongoose');
+const http_status_text = require('./utils/HttpStatusText');
+const admin_routes = require('./routes/Admin');
 
-const express=require('express');
-const url=process.env.MONGO_URL;
-const mongoose=require('mongoose');
-const http_status_text=require('./utils/HttpStatusText');
-const admin_routes=require('./routes/Admin');
+const app = express();
+const url = process.env.MONGO_URL;
 
 app.use(cors());
+app.use(express.json());
 
-try{  
-     mongoose.connect(url).then(()=>{
-    console.log('mongodb srvr start');
-    })
-}catch(err){
-    console.error("DB connection error:", err);
-}
+mongoose.connect(url)
+    .then(() => console.log('db start'))
+    .catch((err) => console.error(err));
 
-
-const app=express();
-
-app.use('/admin',admin_routes)
 app.get("/", (req, res) => {
-  res.json({ message: "Academy API is running 🚀" });
+    res.json({ msg: "API running" });
 });
 
-app.use((req,res)=>{
+app.use('/admin', admin_routes);
+
+app.use((req, res) => {
     res.status(404).json({
-        status:"FAIL",
-        message:"Rout not found"
+        status: "FAIL",
+        msg: "Not found"
     });
-})
-app.use((err,req,res,next)=>{
-    res.status(err.status_code||500).json({
-        status:err.status_text||http_status_text.ERORR,
-        message:err.message,
-        code:err.status_code||500
+});
+
+app.use((err, req, res, next) => {
+    res.status(err.status_code || 500).json({
+        status: err.status_text || http_status_text.ERROR || "ERROR",
+        msg: err.message,
+        code: err.status_code || 500
     });
-    next();
-})
+});
 
 module.exports = app;
