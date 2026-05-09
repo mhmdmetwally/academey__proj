34121fc67674c02fd218f99647cb2fea0e47e83b
mhmdmetwally <mhmdedak@gmail.com>
2026-05-09 14:40:01 +0200

    check app on vercel

--- controllers/Academy.js
+++ controllers/Academy.js
@@ -1,2 +1,15 @@
 const AsyncWrapper = require('../middleware/AsyncWrapper');
 const User = require('../models/User');
+
+
+
+
+const patchActiveSupervisor = AsyncWrapper(
+    async(req,res,next)=>{
+        const {phone,name}=req.body;
+        const user = await User.findOne({phone});
+        if(!user){
+            
+        }
+    }
+)
