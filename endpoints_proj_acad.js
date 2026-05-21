94b92f04acb62e5ad1db7ecbea190468385db497
mhmdmetwally <mhmdedak@gmail.com>
2026-05-21 14:01:43 +0200

    solve mongo prob

---
+++ endpoints_proj_acad.js
@@ -0,0 +1,15 @@
+/academy/register
+/academy/login
+/academy/supervisor/active
+/academy/supervisor/stop
+
+
+/user/register
+/user/login
+
+
+/admin/academy/student_count
+/admin/academy/showallacademy
+/admin/academy/singleacademy
+/academy/academy/stop
+/academy/academy/active
