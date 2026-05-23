915f4c8bdaf0c4349206d9c28da8f52fb2eca8f5
mhmdmetwally <mhmdedak@gmail.com>
2026-05-23 05:57:22 +0200

    solve mongo prob

--- controllers/Academy.js
+++ controllers/Academy.js
@@ -5,6 +5,8 @@ const app_error = require('../utils/AppError');
 const http_status_text = require('../utils/HttpStatusText');
 const salt_round = Number(process.env.salt_round);
 const user_role = require('../utils/UserRole');
+const bcrypt = require('bcrypt');
+const salt_round = Number(process.env.salt_round);
 
 const patchActiveSupervisor = AsyncWrapper(
     async(req,res,next)=>{
