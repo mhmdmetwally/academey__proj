6ff7606123b67887f8d86fc26090bddfac235fb5
mhmdmetwally <mhmdedak@gmail.com>
2026-08-10 02:34:56 +0200

    add pass to acd module

--- models/Academy.js
+++ models/Academy.js
@@ -36,7 +36,30 @@ const AcademySchema = new mongoose.Schema(
             unique: true,
             trim: true
         },
-
+        password: {
+            type: String,
+            required: true,
+    
+            validate: {
+                validator: function(value) {
+    
+                    return validator.isStrongPassword(
+                        value,
+                        {
+                            minLength: 8,
+                            minLowercase: 1,
+                            minUppercase: 1,
+                            minNumbers: 1,
+                            minSymbols: 1
+                        }
+                    );
+    
+                },
+    
+                message:
+                    'كلمة المرور ضعيفة! يجب أن تحتوي على 8 أحرف تشمل (حرف كبير، حرف صغير، رقم، ورمز)'
+            }
+        },
         is_active: {
             type: Boolean,
             default: false
