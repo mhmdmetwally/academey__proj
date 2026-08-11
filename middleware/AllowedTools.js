3c81c8b68f7f1434f01d44dc702930a5c69ce182
mhmdmetwally <mhmdedak@gmail.com>
2026-08-11 15:54:06 +0200

    is_active

--- middleware/AllowedTools.js
+++ middleware/AllowedTools.js
@@ -1,35 +1,83 @@
-const app_error=require('../utils/AppError');
-const http_status_text=require('../utils/HttpStatusText')
 
-module.exports = (...allowedroles)=>{
-    return (req,res,next)=>{
-        const cur_role=req.user.role;
-        const is_active = req.user.is_active;
-        
-        if(!is_active)
-        {
-             const error =
+const app_error =
+    require('../utils/AppError');
+
+const http_status_text =
+    require('../utils/HttpStatusText');
+
+
+module.exports = (...allowedroles) => {
+
+    return (req, res, next) => {
+
+        const cur_role =
+            req.user.role;
+
+
+        /*
+        Check Active Status
+
+        is_active must come from VerifyToken
+        after checking the database.
+
+        It should NOT come from the JWT itself.
+        */
+
+        if (
+            req.user.is_active === false
+        ) {
+
+            const error =
                 new app_error();
 
             error.create(
-                
-                `${cur_role} is not active`+ req.user,
+
+                `${cur_role} is not active`,
+
                 403,
+
                 http_status_text.FAIL
+
             );
 
             return next(error);
+
         }
 
-        if(allowedroles.includes(cur_role)){
+
+        /*
+        Check Role
+        */
+
+        if (
+            allowedroles.includes(cur_role)
+        ) {
+
             return next();
+
         }
-        const err=new app_error();
-        err.create(
+
+
+        /*
+        Role Not Allowed
+        */
+
+        const error =
+            new app_error();
+
+        error.create(
+
             `${cur_role} not allowed to make this action`,
+
             403,
+
             http_status_text.FAIL
+
         );
-        return next(err);
+
+        return next(error);
+
     };
-}
+
+};
+
