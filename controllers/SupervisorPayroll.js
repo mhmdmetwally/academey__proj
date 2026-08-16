7fb71fb1cd0fcbb80f7ba7618683f111e8fe8bad
mhmdmetwally <mhmdedak@gmail.com>
2026-08-16 22:25:35 +0200

    get all payrolls

--- controllers/SupervisorPayroll.js
+++ controllers/SupervisorPayroll.js
@@ -214,9 +214,14 @@ const getSupervisorPayrolls = AsyncWrapper(async (req, res, next) => {
     if (status) filter.status = status;
 
     const payrolls = await SupervisorPayroll.find(filter)
-        .populate('supervisor', 'name phone')
+        .populate({
+            path: 'supervisor',
+            populate: {
+                path: 'user',
+                select: 'name phone'
+            }
+            })
         .sort({ createdAt: -1 });
-
     return res.status(200).json({
         status: http_status_text.SUCCESS,
         data: { payrolls }
